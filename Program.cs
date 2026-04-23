43d51b07549d1a02895fd756332f88ad2538ad58
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-04-23 10:23:44 +0800

    hubh

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ImageProcessor myimagejohndoe = new ImageProcessor();
+
+myimagejohndoe.resize(5);
+myimagejohndoe.resize(150);
+myimagejohndoe.resize(450,550);
+myimagejohndoe.resize("A5");
+class ImageProcessor
+{
+    public void resize(int size)
+    {
+        if (size <= 0)
+        {
+            Console.WriteLine("THATS TOO SMALL");
+        }
+        else if (size > 100)
+        {
+            Console.WriteLine($"SIZE (ENLARGED) CHANGED TO: {size}%");
+        }
+        else if (size < 100)
+        {
+            Console.WriteLine($"SIZE (SHRINKED) CHANGED TO: {size}%");
+        }
+        else
+        {
+            Console.WriteLine("NOTHING CHANGED");
+        }
+    }
+    public void resize(int width, int height)
+    {
+        Console.WriteLine($"SIZE CHANGED TO {width} {height}");
+    }
+    public void resize(string PaperSize)
+    {
+        Console.WriteLine($"SIZE ADJUSTED TO {PaperSize}");
+    }
+}
